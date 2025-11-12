85c1d61dd09a98bba3d1ac1519dfe70efb5ec657
copelansam <copelansam@gmail.com>
2025-11-12 12:29:23 -0500

    Update decpendencites and get project to a point where we can run it in a browser

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -1,4 +1,33 @@
 package com.example.ecommerce;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.io.IOException;
+import java.awt.*;
+
+@SpringBootApplication
 public class EcommerceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EcommerceApplication.class,args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void openBrowser(){
+        try{
+            String url = "http://localhost:8080";
+            if (Desktop.isDesktopSupported()){
+                Desktop.getDesktop().browse(new URI(url));
+            }
+        }
+        catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
