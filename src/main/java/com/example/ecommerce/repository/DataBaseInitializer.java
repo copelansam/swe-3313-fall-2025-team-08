d35385232e0172f66c131a21d8b1aec551852af7
copelansam <copelansam@gmail.com>
2025-11-17 17:40:15 -0500

    Update database intializer

--- src/main/java/com/example/ecommerce/repository/DataBaseInitializer.java
+++ src/main/java/com/example/ecommerce/repository/DataBaseInitializer.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.repository;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -7,11 +8,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataBaseInitializer {
 
+    // initializes the database by creating all of the tables if they do not exist
+
     private final JdbcTemplate jdbc;
 
-    public DataBaseInitializer(JdbcTemplate jdbc){
+
+
+    public DataBaseInitializer(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
+    }
+
+    @PostConstruct
+    public void init() {
 
         jdbc.execute();
     }
+
 }
