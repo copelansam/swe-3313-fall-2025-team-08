4bee9d8c87aeded1566a46619dc1b936bb720c21
copelansam <copelansam@gmail.com>
2025-11-17 22:45:45 -0500

    Reformat queries to be easier to read

--- src/main/java/com/example/ecommerce/repository/DatabaseInitializer.java
+++ src/main/java/com/example/ecommerce/repository/DatabaseInitializer.java
@@ -18,21 +18,75 @@ public class DatabaseInitializer {
     @PostConstruct
     public void init() {
 
-        jdbc.execute( "CREATE TABLE IF NOT EXISTS User ( userid    integer PRIMARY KEY, ame      varchar(100), userName  varchar(100), email     varchar(100), password  varchar(100), " +
-                "isAdmin   boolean );" );
-        jdbc.execute("CREATE TABLE IF NOT EXISTS Shipping_Address ( streetAddress varchar(100) PRIMARY KEY, city          varchar(50), state         char(2), zip           " +
-                "char(5) );");
-        jdbc.execute("CREATE TABLE IF NOT EXISTS Order ( orderid           integer PRIMARY KEY, userid            int NOT NULL, orderDate         date, subTotal          numeric, shippingPrice     numeric, taxes             numeric, grandTotal        numeric, shippingSelection varchar(100), " +
-                "FOREIGN KEY (userid) references User(userid) );");
-        jdbc.execute("CREATE TABLE IF NOT EXISTS Item( itemid       integer PRIMARY KEY, name         varchar(100), description  varchar(1000), imagePath    varchar(50), price        numeric, " +
-                "inStock      boolean );");
-        jdbc.execute("CREATE TABLE IF NOT EXISTS Card( creditCardNumber char(16) PRIMARY KEY, expirationMonth  char(2), expirationYear   char(2), " +
-                "securityCode     char(3) );");
-        jdbc.execute("CREATE TABLE IF NOT EXISTS Order_Address ( orderid               integer, streetAddress         integer, PRIMARY KEY (orderid, streetAddress), FOREIGN KEY (orderid) references Order(orderid), " +
-                "FOREIGN KEY (streetAddress) references Shipping_Address(streetAddress) );");
-        jdbc.execute("CREATE TABLE IF NOT EXISTS Order_Card  ( orderid               integer, creditCardNumber      integer, PRIMARY KEY (orderid, creditCardNumber), FOREIGN KEY (orderid) references Order(orderid), " +
-                "FOREIGN KEY (creditCardNumber) references Card(creditCardNumber) );");
-        jdbc.execute("CREATE TABLE IF NOT EXISTS Order_Line ( itemid      integer, orderid     integer, PRIMARY KEY (itemid, orderid), FOREIGN KEY (itemid) references Item(itemid), " +
-                "FOREIGN KEY (orderid) references Order(orderid) );");
+        // SQLite will treat integer primary key as an alias for autoincrement. Meaning that if it is not specified
+        // then it will automatically assign it an unused value
+
+        jdbc.execute( "CREATE TABLE IF NOT EXISTS User ( " +
+                "userId    integer PRIMARY KEY, " +
+                "name      varchar(100), " +
+                "userName  varchar(100), " +
+                "email     varchar(100), " +
+                "password  varchar(100), " +
+                "isAdmin   boolean " +
+                ");" );
+
+        jdbc.execute("CREATE TABLE IF NOT EXISTS Shipping_Address (" +
+                "streetAddress varchar(100) PRIMARY KEY, " +
+                "city          varchar(50), " +
+                "state         char(2), " +
+                "zip           char(5) " +
+                ");");
+
+        jdbc.execute("CREATE TABLE IF NOT EXISTS \"Order\" ( " +
+                "orderId           integer PRIMARY KEY, " +
+                "userId            int NOT NULL, " +
+                "orderDate         date, " +
+                "subTotal          numeric, " +
+                "shippingPrice     numeric, " +
+                "taxes             numeric, " +
+                "grandTotal        numeric, " +
+                "shippingSelection varchar(100), " +
+                "FOREIGN KEY (userid) references User(userid) " +
+                ");");
+
+        jdbc.execute("CREATE TABLE IF NOT EXISTS Item( " +
+                "itemId       integer PRIMARY KEY, " +
+                "name         varchar(100), " +
+                "description  varchar(1000), " +
+                "imagePath    varchar(50), " +
+                "price        numeric, " +
+                "inStock      boolean " +
+                ");");
+
+        jdbc.execute("CREATE TABLE IF NOT EXISTS Card( " +
+                "creditCardNumber char(16) PRIMARY KEY, " +
+                "expirationMonth  char(2), " +
+                "expirationYear   char(2), " +
+                "securityCode     char(3) " +
+                ");");
+
+        jdbc.execute("CREATE TABLE IF NOT EXISTS Order_Address ( " +
+                "orderId               integer," +
+                "streetAddress         integer, " +
+                "PRIMARY KEY (orderId, streetAddress)," +
+                " FOREIGN KEY (orderId) references Order(orderId), " +
+                "FOREIGN KEY (streetAddress) references Shipping_Address(streetAddress) " +
+                ");");
+
+        jdbc.execute("CREATE TABLE IF NOT EXISTS Order_Card  ( " +
+                "orderId               integer," +
+                " creditCardNumber      integer," +
+                " PRIMARY KEY (orderId, creditCardNumber), " +
+                "FOREIGN KEY (orderId) references Order(orderId), " +
+                "FOREIGN KEY (creditCardNumber) references Card(creditCardNumber) " +
+                ");");
+
+        jdbc.execute("CREATE TABLE IF NOT EXISTS Order_Line ( " +
+                "itemId      integer, " +
+                "orderId     integer, " +
+                "PRIMARY KEY (itemId, orderId), " +
+                "FOREIGN KEY (itemId) references Item(itemId), " +
+                "FOREIGN KEY (orderId) references Order(orderId) " +
+                ");");
     }
 }
--- src/main/java/com/example/ecommerce/repository/UserEntityRepository.java
+++ src/main/java/com/example/ecommerce/repository/UserEntityRepository.java
@@ -12,5 +12,6 @@ public class UserEntityRepository {
     public UserEntityRepository(JdbcTemplate jdbc){
 
         this.jdbc = jdbc;
+
     }
 }
