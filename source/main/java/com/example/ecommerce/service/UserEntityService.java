f629954a51aa192d2380bf78644280631afad2e4
Elijah-Stults <elijah.stults@yahoo.com>
2025-11-20 19:00:38 -0500

    Added seed data to user table

--- source/main/java/com/example/ecommerce/service/UserEntityService.java
+++ source/main/java/com/example/ecommerce/service/UserEntityService.java
@@ -13,6 +13,11 @@ public class UserEntityService {
     public UserEntityService(UserEntityRepository userTable){
 
         this.userTable = userTable;
+
+        //Seed Data for the userTable
+        userTable.addRow("elijah", "ElijahStults", "Elijah123@yahoo.com", "elijah1234$");
+        userTable.addRow("john", "JohnDoe", "JohnD@yahoo.com", "john1234$");
+        userTable.addRow("annie", "AnnieDoe", "AnnieD@yahoo.com", "annie1234$");
     }
 
     public UserRegistrationResult registerUser(String name, String username, String email, String password, String passwordConfirm){
