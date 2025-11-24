d8e5c96dc777f5710430edfcf98abcd864576361
Nylah B <nylahb7@gmail.com>
2025-11-23 23:58:21 -0500

    Update Order.java

--- source/main/java/com/example/ecommerce/model/Order.java
+++ source/main/java/com/example/ecommerce/model/Order.java
@@ -1,4 +1,26 @@
 package com.example.ecommerce.model;
 
+import java.math.BigDecimal;
+
 public class Order {
+    private String creditCardNumber;
+    private String shippingOption;
+    private BigDecimal shippingCost;
+    private BigDecimal grandTotal;
+    private BigDecimal subtotal;
+    private BigDecimal taxes;
+
+    public Order( ){
+
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public String getShippingOption() {
+        return shippingOption;
+    }
+
+
 }
