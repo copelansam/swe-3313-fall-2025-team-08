7137b8e0d96c97c0d6d50dac749d5aea145973c5
Nylah B <nylahb7@gmail.com>
2025-11-24 00:20:05 -0500

    Update Cart.java

--- source/main/java/com/example/ecommerce/model/Cart.java
+++ source/main/java/com/example/ecommerce/model/Cart.java
@@ -1,4 +1,24 @@
 package com.example.ecommerce.model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class Cart {
+    private ArrayList<Item> items;
+    private String shippingOption;
+    private BigDecimal grandTotal;
+    private BigDecimal subtotal;
+    private BigDecimal taxes;
+
+    public Cart(){
+
+    }
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+
+    public void removeItem(Item item){
+        items.remove(item);
+    }
 }
