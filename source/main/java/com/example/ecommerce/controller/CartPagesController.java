4b1d8aa0c628d7d08dcb21ea52485a6286e9210d
copelansam <copelansam@gmail.com>
2025-11-30 01:47:09 -0500

    Update payment page to display cart info

--- source/main/java/com/example/ecommerce/controller/CartPagesController.java
+++ source/main/java/com/example/ecommerce/controller/CartPagesController.java
@@ -1,6 +1,8 @@
 package com.example.ecommerce.controller;
 
 
+import com.example.ecommerce.model.Cart;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,11 @@ public class CartPagesController {
 
 
     @GetMapping("/payment")
-    public String payment(){
+    public String payment(Model model, HttpSession session){
+
+        Cart cart = (Cart) session.getAttribute("cart");
+
+        model.addAttribute("cart", cart);
 
         return "payment";
     }
